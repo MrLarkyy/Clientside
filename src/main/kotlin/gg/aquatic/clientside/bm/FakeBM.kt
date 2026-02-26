e4e655863482bb904ce994b4e5d8366c42128081
MrLarkyy_ <studiotadikl@gmail.com>
2026-02-26 09:54:09 +0100

    Enhance `FakeBM` interaction handling by adding `onInteract` event and hitbox listener; update `bettermodel-bukkit-api` to `2.1.0`

--- src/main/kotlin/gg/aquatic/clientside/bm/FakeBM.kt
+++ src/main/kotlin/gg/aquatic/clientside/bm/FakeBM.kt
@@ -1,22 +1,28 @@
 package gg.aquatic.clientside.bm
 
 import gg.aquatic.clientside.FakeObject
+import gg.aquatic.clientside.ObjectInteractEvent
+import gg.aquatic.clientside.entity.FakeEntity
 import gg.aquatic.common.audience.AquaticAudience
 import kr.toxicity.model.api.BetterModel
 import kr.toxicity.model.api.animation.AnimationModifier
 import kr.toxicity.model.api.bukkit.platform.BukkitAdapter
 import kr.toxicity.model.api.bukkit.platform.BukkitPlayer
+import kr.toxicity.model.api.event.hitbox.HitBoxInteractEvent
+import kr.toxicity.model.api.nms.ModelInteractionHand
 import kr.toxicity.model.api.tracker.TrackerModifier
 import kr.toxicity.model.api.tracker.TrackerUpdateAction
 import org.bukkit.Color
 import org.bukkit.Location
 import org.bukkit.entity.Player
 
+
 class FakeBM(
     override val location: Location,
     val modelId: String,
     override val viewRange: Int,
     initialAudience: AquaticAudience,
+    var onInteract: ObjectInteractEvent<FakeBM> = { _, _, _ -> },
 ) : FakeObject(viewRange, initialAudience) {
 
     val model = BetterModel.model(modelId).get().create(BukkitAdapter.adapt(location), TrackerModifier.DEFAULT).apply {
@@ -26,13 +32,20 @@ class FakeBM(
     }
 
     override fun register() {
+        model.listenHitBox(HitBoxInteractEvent::class.java) { e ->
+            val isLeft = e.hand == ModelInteractionHand.LEFT
 
+            val player = e.who as BukkitPlayer
+            handleInteract(player.source(), isLeft)
+        }
     }
 
     override fun onShow(player: Player) {}
     override fun onHide(player: Player) {}
 
-    override fun handleInteract(player: Player, isLeftClick: Boolean) {}
+    override fun handleInteract(player: Player, isLeftClick: Boolean) {
+        onInteract.onInteract(this, player, isLeftClick)
+    }
 
     @Suppress("unused")
     fun setTint(tint: Color) {
